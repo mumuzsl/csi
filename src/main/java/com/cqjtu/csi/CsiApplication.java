d0571145ff981fdde9e6018764d1ee3f846be57f
mumuzsl <1445600106@qq.com>
2020-01-11 10:49:51 +0800

    Initial commit

---
+++ src/main/java/com/cqjtu/csi/CsiApplication.java
@@ -0,0 +1,13 @@
+package com.cqjtu.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cqjtu/csi/CsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqjtu.csi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
