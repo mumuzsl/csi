aa0d893687ff495acb375853d3112191be30c88a
mumuzsl <1445600106@qq.com>
2020-01-11 15:23:32 +0800

    add alibaba druid

--- src/main/java/com/cqjtu/csi/CsiApplication.java
+++ src/main/java/com/cqjtu/csi/CsiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CsiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CsiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CsiApplication.class, args);
+    }
 
 }
