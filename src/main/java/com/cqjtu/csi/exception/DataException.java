cd66a9909167b17d74563c546212991b947f530a
mumuzsl <1445600106@qq.com>
2020-02-19 23:31:14 +0800

    del .mvn dir

---
+++ src/main/java/com/cqjtu/csi/exception/DataException.java
@@ -0,0 +1,8 @@
+package com.cqjtu.csi.exception;
+
+/**
+ * @author mumu
+ * @date 2020/2/18
+ */
+public class DataException {
+}
---
+++ src/main/java/com/cqjtu/csi/utils/HttpUtils.java
@@ -0,0 +1,8 @@
+package com.cqjtu.csi.utils;
+
+/**
+ * @author mumu
+ * @date 2020/2/18
+ */
+public class WebUtils {
+}
