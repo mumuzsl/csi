d2f308339b76fae16624f79a2ed2d03c3a175290
mumuzsl <1445600106@qq.com>
2022-02-07 14:23:22 +0800

    升级组件版本，消除安全隐患。#5

--- src/main/java/com/cqjtu/csi/CsiApplication.java
+++ src/main/java/com/cqjtu/csi/CsiApplication.java
@@ -6,12 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 
+/**
+ * @author mumu
+ */
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.cqjtu.csi.repository", repositoryBaseClass = BaseRepositoryImpl.class)
 @EnableAsync
 @EnableScheduling
+@EnableOpenApi
 public class CsiApplication {
 
     public static void main(String[] args) {
